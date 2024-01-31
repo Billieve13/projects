b8394f3eccc9580a9d016ad39ec26629c76d7499
Billieve13 <rmr837834@gmail.com>
2024-01-31 17:29:51 -0500

    push 4 with .gitignore and .env

--- ClassProjects/knex_test/knexfile.js
+++ ClassProjects/knex_test/knexfile.js
@@ -1,20 +1,22 @@
 // Update with your config settings.
-
+require('dotenv').config()
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const connectionString = process.env.DB_CONNECT_STRING;
 module.exports = {
 
   development: {
-    client: 'postgresql', //pg
-    connection: {
-      host: '127.0.0.1',
-      password: 'docker',
-      user: 'postgres',
-      port: 5432,
-      database: 'pet_store'
-    }
-    //'postgres://postgres:docker@localhost/pg-docker'
+    client: 'pg', //postgresql
+    connection: connectionString//{
+      //host: '127.0.0.1',
+      //password: 'docker',
+      //user: 'postgres',
+      //port: 5432
+      //database: 'pet_store'
+    //}
+    //'postgresql://postgres:docker@localhost/pg-docker'
   },
 
   staging: {
