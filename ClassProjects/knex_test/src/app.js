4df30a30d9efaf35fc3c9d65f403cdd4a1d9e643
Billieve13 <rmr837834@gmail.com>
2024-01-31 17:04:41 -0500

    push 2 - knex PE

--- ClassProjects/knex_test/src/app.js
+++ ClassProjects/knex_test/src/app.js
@@ -14,33 +14,6 @@ app.get('/pets', (request, response) => {
     .then(pets => {
       var petNames = pets.map(pet => pet.name)
       response.json(petNames);
+      //console.log(process.env)
     })
 })
-
-// app.listen(port, () => {
-//   console.log(`Your server is running on port ${port}`)
-// })
-
-// const express = require('express');
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-// const knex = require('knex')(require('path/to/knexfile.js')[process.env.NODE_ENV||'development']);
-
-// app.use(express.json());
-
-// app.get('/pets', function(req, res) {
-//   knex('pet')
-//     .select('*')
-//     .then(data => res.status(200).json(data))
-//     .catch(err =>
-//       res.status(404).json({
-//         message:
-//           'The data you are looking for could not be found. Please try again'
-//       })
-//     );
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`The server is running on ${PORT}`);
-// });
